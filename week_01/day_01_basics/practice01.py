653289e7276243108a14036777e7c9abf3de04b8
Bhavana Chintala <bhavanachintala6371@gmail.com>
2026-07-05 22:43:50 +0530

    Add practice01.py file

---
+++ week_01/day_01_basics/practice01.py
@@ -0,0 +1,16 @@
+# Day 01 part 01: python intro,get started,syntax,output, and comments
+print("Hello Python!")
+print("My name is Bhavana")
+print("I am learning python")
+print("this is day 01 of my python practice")
+
+print(10 + 20)
+print(50 - 40)
+print(20 * 10)
+print(40 / 20)
+
+print("10 + 20")
+print(10 + 20)
+
+# This is a simple practice program from day 01 learnings
+print("day 01 part 01 of python learning is completed successfully")
