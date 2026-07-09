6170a5b920c3e2d176b722052ed58dd4d7f38486
Bhavana Chintala <bhavanachintala6371@gmail.com>
2026-07-09 21:14:35 +0530

    Update practice03.py with string indexing examples

---
+++ week_01/day_01_basics/practice03.py
@@ -0,0 +1,12 @@
+name = "Bhavana"
+# Accessing the characters of a string using indexing
+print(name[0])
+print(name[1], name[-1], name[-7])
+print(name[-0])
+
+name = "Bhavana Chintala"
+aim = "Getting a job in python ASAP"
+# using len() function to get the length of the string
+print(len(name))
+# demonstration of len() function can't be used to get the no.of words in a string
+print(len(aim))
