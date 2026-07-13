6b10eba329a4d408b75e84edc0966e3124638825
Bhavana Chintala <bhavanachintala6371@gmail.com>
2026-07-13 22:19:07 +0530

    Adding list

---
+++ week_01/day_02/list.py
@@ -0,0 +1,13 @@
+numbers = [10, 20, 30, 20]
+numbers.append(40)
+print(numbers)
+print(numbers.count(20))
+print(numbers.index(30))
+numbers.remove(20)
+print(numbers)
+numbers.reverse()
+print(numbers)
+list1 = ["apple", "banana"]
+list2 = ["cherry", "orange"]
+list1.extend(list2)
+print(list1)
