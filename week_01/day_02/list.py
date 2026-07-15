e64b5147f0fafe23e1ddbb4519a957aafc0b1f4a
Bhavana Chintala <bhavanachintala6371@gmail.com>
2026-07-15 22:49:53 +0530

    Adding list.py file

--- week_01/day_02/list.py
+++ week_01/day_02/list.py
@@ -1,18 +1,58 @@
-numbers = [10, 20, 30, 20]
-numbers.append(40)
-print(numbers)
-print(numbers.count(20))
-print(numbers.index(30))
-numbers.remove(20)
-print(numbers)
-numbers.reverse()
-print(numbers)
-list1 = ["apple", "banana"]
-list2 = ["cherry", "orange"]
-list1.extend(list2)
-print(list1)
+# Creating List with same and different values
 fruits = ["apple", "banana", "cherry"]
 numbers = [10, 20, 30]
 mixed = ["Bhavana", 25, True, 99.5]
 print(len(fruits))
 print(type(fruits))
+
+# Accessing list items
+fruits = ["custard", "mulberry", "guava", "apple", "orange", "banana","mango", "grapes"]
+print(fruits[0])
+print(fruits[1])
+print(fruits[2])
+print(fruits[-3])
+print(fruits[-2])
+print(fruits[-1])
+print(fruits[4])
+print(fruits[1:7])
+print(fruits[:8])
+print(fruits[0:])
+print(fruits[-8:-1])
+print("banana" in fruits)
+print("papaya" not in fruits)
+print("grapes" not in fruits)
+
+# Changing the list items
+fruits[0] = "banana"
+print(fruits)
+fruits[1:4] = ["blueberry", "cherry", "strawberry"]
+print(fruits)
+# replacing one item with multiple items
+fruits[1:2] = ["watermelon", "jackfruit", "sapota"]
+print(fruits)
+# replacing multiple items with one item
+fruits[4:8] = ["sapota"]
+print(fruits)
+# adding list items
+# Inserting without replacing
+fruits.insert(0, "jackfruit")
+print(fruits)
+fruits.append("pineapple")
+print(fruits)
+more_fruits = ["apple", "orange", "cherry"]
+fruits.extend(more_fruits)
+print(fruits)
+
+# removing list items
+# remove() deletes first matching value
+fruits.remove("sapota")
+print(fruits)
+# pop() with index removes the item at index
+fruits.pop(1)
+print(fruits)
+# pop() without index specification it removes last item
+fruits.pop()
+print(fruits)
+# del to delete the item from specific index
+del fruits[0]
+print(fruits)
