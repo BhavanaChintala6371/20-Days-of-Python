bbe3cfd59ec8aefdae9470880e9662f2cd9c52e4
Bhavana Chintala <bhavanachintala6371@gmail.com>
2026-07-19 22:10:28 +0530

    adding string.py file

---
+++ week_01/strings.py
@@ -0,0 +1,12 @@
+# Getting started with strings
+message = """ Hello Python!', Learning a programming language successfully within short period is not an easy task"""
+print(message)
+my_message = "bobby's world"
+print(my_message)
+# len() function example
+print(len(my_message))
+# Accessing the individual characters of a string using index
+print(my_message[0]) 
+print(my_message[12])
+# Accessing the range of characters
+print(my_message[0:7])
