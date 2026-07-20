c3f3d4d8d5b3dbc3d7448e66b248279743fbd1d7
Bhavana Chintala <bhavanachintala6371@gmail.com>
2026-07-20 12:11:01 +0530

    Adding files

---
+++ week_01/day_02/data_types.py
@@ -0,0 +1,47 @@
+num = 3.14
+print(type(num))
+
+# Arithmetic operators
+print(3 + 2)
+print(3 - 2)
+print(3 / 2)
+print(3 * 2)
+print(3 // 2)
+print(3 ** 2) 
+print(3 % 2)
+
+# Assignment operators
+num = 5
+num = num + 5
+print(num)
+num += 5
+print(num)
+
+num = num * 5
+print(num)
+num *= 5
+print(num)
+
+# built-in functions to work with numbers
+print(abs(-3))
+print(round(3.175))
+print(round(3.175, 1))
+
+# Comparison operators
+num_1 = 3
+num_2 = 4
+print(num_1 == num_2)
+print(num_1 != num_2)
+print(num_1 >= num_2)
+print(num_1 <= num_2)
+print(num_1 < num_2)
+print(num_2 > num_1)
+
+# casting
+number_1 = "100"
+number_2 = "200"
+print(number_1 + number_2)
+
+number_1 = int(number_1)
+number_2 = int(number_2)
+print(number_1 + number_2)
--- week_01/strings.py
+++ week_01/strings.py
@@ -1,7 +1,7 @@
 # Getting started with strings
 message = """ Hello Python!', Learning a programming language successfully within short period is not an easy task"""
 print(message)
-my_message = "bobby's world"
+my_message = "Bobby's World"
 print(my_message)
 # len() function example
 print(len(my_message))
@@ -9,4 +9,22 @@ print(len(my_message))
 print(my_message[0]) 
 print(my_message[12])
 # Accessing the range of characters
-print(my_message[0:7])
+print(my_message[:7])
+print(my_message[:])
+print(my_message[1:])
+print(my_message[1:5])
+# String methods
+print(my_message.lower())
+print(my_message.upper())
+print(my_message.count("Bobby"))
+print(my_message.find("World"))
+print(my_message.find("Bhavana"))
+print(my_message.replace("World", "Home"))
+# String Concatenation
+greeting = "Hello"
+name = "Bhavana"
+print(greeting + ", " + name)
+# Formatted strings
+print(f"{greeting}, {name}")
+print(dir(name))
+print(help(str.lower))
